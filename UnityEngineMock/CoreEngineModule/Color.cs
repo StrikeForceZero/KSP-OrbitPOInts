be1f0569d225f8a229795cba7a2e069820f7297e
bstriker <strikeforcezero@gmail.com>
2023-09-29 00:06:29 -0400

    Add Vector4 and add more operation overrides

--- UnityEngineMock/CoreEngineModule/Color.cs
+++ UnityEngineMock/CoreEngineModule/Color.cs
@@ -41,5 +41,31 @@ namespace UnityEngineMock
         public static Color cyan => new Color(0f, 1f, 1f); // Cyan
         public static Color gray => new Color(0.5f, 0.5f, 0.5f); // Gray
         public static Color clear => new Color(0f, 0f, 0f, 0f); // clear
+
+        public override int GetHashCode() => ((Vector4) this).GetHashCode();
+
+        public override bool Equals(object other) => other is Color other1 && this.Equals(other1);
+
+        public bool Equals(Color other) => this.r.Equals(other.r) && this.g.Equals(other.g) && this.b.Equals(other.b) && this.a.Equals(other.a);
+
+        public static Color operator +(Color a, Color b) => new Color(a.r + b.r, a.g + b.g, a.b + b.b, a.a + b.a);
+
+        public static Color operator -(Color a, Color b) => new Color(a.r - b.r, a.g - b.g, a.b - b.b, a.a - b.a);
+
+        public static Color operator *(Color a, Color b) => new Color(a.r * b.r, a.g * b.g, a.b * b.b, a.a * b.a);
+
+        public static Color operator *(Color a, float b) => new Color(a.r * b, a.g * b, a.b * b, a.a * b);
+
+        public static Color operator *(float b, Color a) => new Color(a.r * b, a.g * b, a.b * b, a.a * b);
+
+        public static Color operator /(Color a, float b) => new Color(a.r / b, a.g / b, a.b / b, a.a / b);
+
+        public static bool operator ==(Color lhs, Color rhs) => ((Vector4) lhs).Equals((Vector4) rhs);
+
+        public static bool operator !=(Color lhs, Color rhs) => !(lhs == rhs);
+
+        public static implicit operator Vector4(Color c) => new Vector4(c.r, c.g, c.b, c.a);
+
+        public static implicit operator Color(Vector4 v) => new Color(v.x, v.y, v.z, v.w);
     }
 }
