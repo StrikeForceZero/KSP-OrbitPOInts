a7a1851fffb35127eb5e350ad5ddc9b1be3b758a
bstriker <strikeforcezero@gmail.com>
2023-09-29 12:54:02 -0400

    feat: introduce `Option` struct to replace `Smooth.Collections`
    Define a new `Option` struct to represent the absence or presence of values, eliminating the reliance on similar structures from `Smooth.Collections`.

---
+++ OrbitPOInts/Option.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace OrbitPOInts
+{
+    public struct Option<T>
+    {
+        private readonly T _value;
+        private readonly bool _hasValue;
+
+        public Option(T value)
+        {
+            _value = value;
+            _hasValue = true;
+        }
+
+        public bool IsSome => _hasValue;
+        public bool IsNone => !_hasValue;
+
+        public T Value
+        {
+            get
+            {
+                if (!_hasValue)
+                    throw new InvalidOperationException("Option does not have a value.");
+                return _value;
+            }
+        }
+
+        public static Option<T> None => new();
+
+        public static implicit operator Option<T>(T value) => new(value);
+    }
+}
