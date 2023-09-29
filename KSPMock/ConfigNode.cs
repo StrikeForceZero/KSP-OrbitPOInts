261af08cbaba72654cf95a175f5994702d0d3f5f
bstriker <strikeforcezero@gmail.com>
2023-09-28 23:53:07 -0400

    Add implementation for ConfigNode

--- KSPMock/ConfigNode.cs
+++ KSPMock/ConfigNode.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KSPMock
 {
     public class ConfigNode
     {
         public string name;
+        private readonly Dictionary<string, string> _values = new();
+        private readonly Dictionary<string, List<ConfigNode>> _nodes = new();
 
         public ConfigNode()
         {
@@ -18,37 +22,44 @@ namespace KSPMock
 
         public void SetValue(string key, string value)
         {
-            throw new NotImplementedException();
+            _values[key] = value;
         }
 
         public string GetValue(string key)
         {
-            throw new NotImplementedException();
+            return _values.TryGetValue(key, out var value) ? value : null;
         }
 
         public void AddValue(string key, object value)
         {
-            throw new NotImplementedException();
+            _values[key] = value.ToString();
         }
 
         public bool TryGetValue(string key, ref string value)
         {
-            throw new NotImplementedException();
+            return _values.TryGetValue(key, out value);
         }
 
         public void AddNode(string key, ConfigNode node)
         {
-            throw new NotImplementedException();
+            if (_nodes.TryGetValue(key, out var nodeList))
+            {
+                nodeList.Add(node);
+            }
+            else
+            {
+                _nodes[key] = new List<ConfigNode> { node };
+            }
         }
 
         public ConfigNode GetNode(string key)
         {
-            throw new NotImplementedException();
+            return _nodes.TryGetValue(key, out var nodeList) ? nodeList.FirstOrDefault() : null;
         }
 
         public ConfigNode[] GetNodes(string key)
         {
-            throw new NotImplementedException();
+            return _nodes.TryGetValue(key, out var nodeList) ? nodeList.ToArray() : Array.Empty<ConfigNode>();
         }
     }
 }
