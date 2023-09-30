8b41e77c607497f3fff3387e372faf3549ba3d8d
bstriker <strikeforcezero@gmail.com>
2023-09-30 13:19:46 -0400

    feat(utils): Add instance-based and resettable version of DelayedAction

--- OrbitPOInts/Utils/DelayedAction.cs
+++ OrbitPOInts/Utils/DelayedAction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 
 namespace OrbitPOInts.Utils
 {
+    // stateless
     public static class DelayedAction
     {
         public delegate void ActionToDelay();
@@ -14,6 +15,48 @@ namespace OrbitPOInts.Utils
             }
 
             action.Invoke();
+        }
+    }
+
+    // instance based / resettable
+    // TODO: determine if this will ever be needed/useful
+    public class DelayedActionCoroutine : IEnumerator
+    {
+        public delegate void ActionToDelay();
+
+        private readonly ActionToDelay _action;
+        private readonly uint _framesToDelay;
+        private uint _frameCount;
+
+        public DelayedActionCoroutine(ActionToDelay action, uint framesToDelay)
+        {
+            _action = action;
+            _framesToDelay = framesToDelay;
+            _frameCount = 0;
+        }
+
+        public bool MoveNext()
+        {
+            if(_frameCount >= _framesToDelay)
+            {
+                _action.Invoke();
+                return false; // Stops the coroutine
+            }
+            _frameCount++;
+            return true; // Continues to the next iteration
+        }
+
+        public void Reset()
+        {
+            // Typically not used in Unity coroutines
+            _frameCount = 0;
+        }
+
+        public object Current => null; // Yielded value, typically null for Unity coroutines waiting for the next frame
+
+        public static IEnumerator Create(ActionToDelay action, uint framesToDelay = 1)
+        {
+            return new DelayedActionCoroutine(action, framesToDelay);
         }
     }
 }
