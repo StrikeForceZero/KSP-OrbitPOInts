5e967477f1470d4c9508761fb96c1af3e0283495
bstriker <strikeforcezero@gmail.com>
2023-09-30 17:28:36 -0400

    refactor(extensions): Rename ListExtensions to EnumerableExtensions

--- OrbitPOInts/Extensions/EnumerableExtensions.cs
+++ OrbitPOInts/Extensions/EnumerableExtensions.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace OrbitPOInts.Extensions
 {
-    public static class ListExtensions
+    public static class EnumerableExtensions
     {
         public static IEnumerable<(int Index, T Value)> WithIndex<T>(this List<T> list)
         {
