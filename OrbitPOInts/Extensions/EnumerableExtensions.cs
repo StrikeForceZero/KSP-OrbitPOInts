9f340c3c3798533f3a67de8538cdb782777b6b18
bstriker <strikeforcezero@gmail.com>
2023-09-30 17:32:57 -0400

    refactor(extensions): Make WithIndex more general by returning IEnumerable

--- OrbitPOInts/Extensions/EnumerableExtensions.cs
+++ OrbitPOInts/Extensions/EnumerableExtensions.cs
@@ -4,12 +4,9 @@ namespace OrbitPOInts.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<(int Index, T Value)> WithIndex<T>(this List<T> list)
+        public static IEnumerable<(int Index, T Value)> WithIndex<T>(this IEnumerable<T> source)
         {
-            for (int i = 0; i < list.Count; i++)
-            {
-                yield return (i, list[i]);
-            }
+            return source.Select((t, i) => (i, t));
         }
     }
 }
