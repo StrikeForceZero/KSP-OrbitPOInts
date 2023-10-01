4afbb1b7f712c22e82a8ce8a571fcd61fe0b635a
bstriker <strikeforcezero@gmail.com>
2023-09-30 22:36:16 -0400

    feat(poi): add ResettablePoi for use when default POIs get changed

---
+++ OrbitPOInts/Data/POI/ResettablePoi.cs
@@ -0,0 +1,63 @@
+using System.ComponentModel;
+
+namespace OrbitPOInts.Data.POI
+{
+    public class ResettablePoi : POI
+    {
+        public bool Dirty { get; private set; }
+        public bool Sealed { get; private set; }
+        public POI SealedState { get; private set; }
+
+        public ResettablePoi(POI poiSource, bool seal = false) : base(poiSource.Type, poiSource.Body)
+        {
+            CopyFromPoi(poiSource);
+            if (!seal) return;
+            Seal();
+        }
+
+        public ResettablePoi(PoiType type, CelestialBody body = null) : base(type, body)
+        {
+            PropertyChanged += OnBasePropertyChanged;
+        }
+
+        public static ResettablePoi From(POI sourcePoi, bool seal = false)
+        {
+            return new ResettablePoi(sourcePoi, seal);
+        }
+
+        private void OnBasePropertyChanged(object senderPoi, PropertyChangedEventArgs args)
+        {
+            if (!Sealed) return;
+            Dirty = true;
+        }
+
+        public void Seal()
+        {
+            if (Sealed) return;
+            Sealed = true;
+            SealedState = Clone();
+        }
+
+        public void Reset()
+        {
+            if (!Dirty) return;
+
+            CopyFromPoi(SealedState);
+
+            Dirty = false;
+        }
+
+        private void CopyFromPoi(POI poi)
+        {
+            Label = poi.Label;
+            Enabled = poi.Enabled;
+            Radius = poi.Radius;
+            Color = poi.Color;
+            Type = poi.Type;
+            Body = poi.Body;
+            AddPlanetRadius = poi.AddPlanetRadius;
+            LineWidth = poi.LineWidth;
+            Resolution = poi.Resolution;
+        }
+    }
+}
