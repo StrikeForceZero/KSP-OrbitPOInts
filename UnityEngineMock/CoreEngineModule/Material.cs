3c3256f07b943f7a872249108873eadc9553b62d
bstriker <strikeforcezero@gmail.com>
2023-10-09 01:23:42 -0400

    mocks(unity): Add Material constructor stub for Shaders

--- UnityEngineMock/CoreEngineModule/Material.cs
+++ UnityEngineMock/CoreEngineModule/Material.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace UnityEngineMock
 {
     public class Material
     {
-        
+        public Material(Shader shader)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
