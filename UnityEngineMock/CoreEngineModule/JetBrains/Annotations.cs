2e7844c9a211e32cda2029d8c9f2b7390dd2db05
bstriker <strikeforcezero@gmail.com>
2023-10-09 01:23:05 -0400

    mocks(unity): Add JetBrains Annotation NotNullAttribute mock for TEST

--- UnityEngineMock/CoreEngineModule/JetBrains/Annotations.cs
+++ UnityEngineMock/CoreEngineModule/JetBrains/Annotations.cs
@@ -4,4 +4,9 @@ namespace UnityEngineMock.JetBrains.Annotations
 {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.Delegate | AttributeTargets.Field, AllowMultiple = false)]
     public class CanBeNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Parameter | AttributeTargets.Delegate | AttributeTargets.GenericParameter)]
+    public sealed class NotNullAttribute : Attribute
+    {
+    }
 }
