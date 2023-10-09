f7187c560ebe26e3ad0740967629dbeaa7de8ec5
bstriker <strikeforcezero@gmail.com>
2023-10-09 01:21:45 -0400

    mocks(unity): add Shader

---
+++ UnityEngineMock/CoreEngineModule/Shader.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UnityEngineMock
+{
+    public class Shader
+    {
+        public static Shader Find(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
