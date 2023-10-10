79c199b4563d9be9292940b0907f24a7ad2a0ffa
bstriker <strikeforcezero@gmail.com>
2023-10-09 23:22:38 -0400

    feat(cache): Add CelestialBodyCache to reduce lookup times and cache expensive operations on the body

---
+++ OrbitPOInts/CelestialBodyCache.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using OrbitPOInts.Extensions.KSP;
+
+#if TEST
+using System.Linq;
+using KSP_CelestialBody = KSPMock.CelestialBody;
+using KSP_FlightGlobals = KSPMock.FlightGlobals;
+using JB_Annotations = UnityEngineMock.JetBrains.Annotations;
+#else
+using UnityEngine;
+using UniLinq;
+using KSP_CelestialBody = CelestialBody;
+using KSP_FlightGlobals = FlightGlobals;
+using JB_Annotations = JetBrains.Annotations;
+#endif
+
+namespace OrbitPOInts
+{
+    using CelestialBody = KSP_CelestialBody;
+    using FlightGlobals = KSP_FlightGlobals;
+    using CanBeNull = JB_Annotations.CanBeNullAttribute;
+
+    public class CelestialBodyCache
+    {
+        private static CelestialBodyCache _instance;
+        private IReadOnlyDictionary<string, CelestialBody> _nameToBodyDictionary;
+        private IReadOnlyDictionary<CelestialBody, double> _bodyToMaxAltitudeDictionary;
+
+        public static CelestialBodyCache Instance => _instance ??= new CelestialBodyCache();
+        public IReadOnlyDictionary<string, CelestialBody> NameToBodyDictionary => _nameToBodyDictionary ??= new ReadOnlyDictionary<string, CelestialBody>(FlightGlobals.Bodies.ToDictionary(body => body.name));
+        // TODO: might be better to calculate these on demand instead of all on first access
+        public IReadOnlyDictionary<CelestialBody, double> BodyToMaxAltitudeDictionary => _bodyToMaxAltitudeDictionary ??= new ReadOnlyDictionary<CelestialBody, double>(FlightGlobals.Bodies.ToDictionary(body => body, body => body.GetApproxTerrainMaxHeight() + body.Radius));
+
+        private CelestialBodyCache()
+        {
+        }
+
+        [CanBeNull]
+        public CelestialBody ResolveByName(string name)
+        {
+            NameToBodyDictionary.TryGetValue(name, out var result);
+            return result;
+        }
+    }
+}
