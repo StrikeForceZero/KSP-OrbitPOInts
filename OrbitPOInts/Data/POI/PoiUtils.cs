34ced56c3d3e9853171142fb94098be9356f54e9
bstriker <strikeforcezero@gmail.com>
2023-10-10 00:06:32 -0400

    fix(imports): fix conditional imports for TEST configuration

--- OrbitPOInts/Data/POI/PoiUtils.cs
+++ OrbitPOInts/Data/POI/PoiUtils.cs
@@ -5,14 +5,20 @@ using OrbitPOInts.Extensions.KSP;
 #if TEST
 using System.Linq;
 using UnityEngineMock;
+using KSP_CelestialBody = KSPMock.CelestialBody;
+using KSP_FlightGlobals = KSPMock.FlightGlobals;
 #else
 using UniLinq;
 using UnityEngine;
+using KSP_CelestialBody = CelestialBody;
+using KSP_FlightGlobals = FlightGlobals;
 #endif
 
 
 namespace OrbitPOInts.Data.POI
 {
+    using CelestialBody = KSP_CelestialBody;
+    using FlightGlobals = KSP_FlightGlobals;
     using Logger = OrbitPOInts.Utils.Logger;
 
     public static class PoiUtils
